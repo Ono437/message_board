f0e39f0d84937afa2a51b50a9f5846becfd96334
takayuki ono <2023dounyukenshu.103@gmail.com>
2023-05-23 13:50:35 +0900

    フラッシュメッセージの追加

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -29,8 +29,9 @@ public class DestroyServlet extends HttpServlet {
             Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
 
             em.getTransaction().begin();
-            em.remove(m);       // データ削除
+            em.remove(m);
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "削除が完了しました。");
             em.close();
 
             request.getSession().removeAttribute("message_id");
